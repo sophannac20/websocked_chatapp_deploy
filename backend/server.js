4b3824f89b060333a6dcf720ee6a3a50a3f8e420
Sophanna <user@Sophanna.local>
2025-06-07 03:03:41 +0700

    remove *

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
 
